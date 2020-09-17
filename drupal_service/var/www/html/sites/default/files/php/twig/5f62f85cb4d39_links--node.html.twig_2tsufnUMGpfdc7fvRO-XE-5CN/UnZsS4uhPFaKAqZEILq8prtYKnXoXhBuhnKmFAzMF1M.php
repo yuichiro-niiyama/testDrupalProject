e13ea930df38b5c178fd65e23539f0ab7d598715
y.niiyama <y.niiyama@magic-plus.net>
2020-09-17 15:02:04 +0900

    20200917 update drupal9 and add article

---
+++ drupal_service/var/www/html/sites/default/files/php/twig/5f62f85cb4d39_links--node.html.twig_2tsufnUMGpfdc7fvRO-XE-5CN/UnZsS4uhPFaKAqZEILq8prtYKnXoXhBuhnKmFAzMF1M.php
@@ -0,0 +1,93 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/classy/content/links--node.html.twig */
+class __TwigTemplate_5d81ded79444136483d748d32c5e6a88841004f667b4e6e00a4fa7d07786b2f5 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("if" => 36, "include" => 38);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if', 'include'],
+                [],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 36
+        if (($context["links"] ?? null)) {
+            // line 37
+            echo "  <div class=\"node__links\">
+    ";
+            // line 38
+            $this->loadTemplate("links.html.twig", "core/themes/bartik/templates/classy/content/links--node.html.twig", 38)->display($context);
+            // line 39
+            echo "  </div>
+";
+        }
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/classy/content/links--node.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  69 => 39,  67 => 38,  64 => 37,  62 => 36,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/classy/content/links--node.html.twig", "/opt/drupal/web/core/themes/bartik/templates/classy/content/links--node.html.twig");
+    }
+}
---
+++ drupal_service/var/www/html/sites/default/files/php/twig/5f62f85cb4d39_rdf-metadata.html.twig_5MgY18hNRiw3AG5bUVdEK9L-B/bZEviYXXEHvI3PzcvnkT8TQP_XEW5fvgp0xKpEbn5_k.php
@@ -0,0 +1,95 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/bartik/templates/classy/misc/rdf-metadata.html.twig */
+class __TwigTemplate_3226bd8f3b114a546576ca38ece26378da5dcbbac41ce3fdab004f06307660c3 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = array("for" => 18);
+        $filters = array("escape" => 19);
+        $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['for'],
+                ['escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 18
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable(($context["metadata"] ?? null));
+        foreach ($context['_seq'] as $context["_key"] => $context["attributes"]) {
+            // line 19
+            echo "  <span";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(twig_get_attribute($this->env, $this->source, $context["attributes"], "addClass", [0 => "rdf-meta", 1 => "hidden"], "method", false, false, true, 19), 19, $this->source), "html", null, true);
+            echo "></span>
+";
+        }
+        $_parent = $context['_parent'];
+        unset($context['_seq'], $context['_iterated'], $context['_key'], $context['attributes'], $context['_parent'], $context['loop']);
+        $context = array_intersect_key($context, $_parent) + $_parent;
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/bartik/templates/classy/misc/rdf-metadata.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  66 => 19,  62 => 18,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/bartik/templates/classy/misc/rdf-metadata.html.twig", "/opt/drupal/web/core/themes/bartik/templates/classy/misc/rdf-metadata.html.twig");
+    }
+}
